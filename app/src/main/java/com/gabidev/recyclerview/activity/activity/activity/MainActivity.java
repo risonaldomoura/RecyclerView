d266345a378c516c206f1b36188a13a1925ef857
maschinedreams <gabrielle.clima23@gmail.com>
2019-09-10 13:26:45 -0300

    Lista arrumada

--- app/src/main/java/com/gabidev/recyclerview/activity/activity/activity/MainActivity.java
+++ app/src/main/java/com/gabidev/recyclerview/activity/activity/activity/MainActivity.java
@@ -8,20 +8,26 @@ import android.os.Bundle;
 
 import com.gabidev.recyclerview.R;
 import com.gabidev.recyclerview.activity.activity.adapter.Adapter;
+import com.gabidev.recyclerview.activity.activity.model.Filme;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-
+    private List<Filme> listaFilmes =  new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
+        // TO-DO: listagem de filmes
+        this.criarFilmes();
 
         // TO-DO: configurar adapter
-        Adapter adapter = new Adapter();
+        Adapter adapter = new Adapter(listaFilmes);
 
 
         // TO-DO: configurar RecyclerView
@@ -29,5 +35,17 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter( adapter);
+    }
+    public void criarFilmes() {
+    Filme filme = new Filme( "Titanic", "Drama", "1997");
+    this.listaFilmes.add(filme);
+         filme = new Filme( "Era uma vez em hollywood", "Ação", "2019");
+        this.listaFilmes.add(filme);
+        filme = new Filme( "Kill Bill 1", "Ação", "2003");
+        this.listaFilmes.add(filme);
+        filme = new Filme( "Wall Street", "Suspense", "1987");
+        this.listaFilmes.add(filme);
+         filme = new Filme( "The Breakfast Club", "Comédia", "1985");
+        this.listaFilmes.add(filme);
     }
 }
--- app/src/main/java/com/gabidev/recyclerview/activity/activity/adapter/Adapter.java
+++ app/src/main/java/com/gabidev/recyclerview/activity/activity/adapter/Adapter.java
@@ -7,13 +7,23 @@ import android.widget.TextView;
 
 
 import com.gabidev.recyclerview.R;
+import com.gabidev.recyclerview.activity.activity.model.Filme;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+    private  List<Filme> listaFilmes;
 
+
+    public Adapter(List<Filme> lista) {
+        this.listaFilmes = lista;
+
+
+    }
 
     @NonNull
     @Override
@@ -25,15 +35,16 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.titulo .setText("Titulo de teste");
-        holder.genero.setText("Comédia");
-        holder.ano.setText("2017");
+        Filme filme = listaFilmes.get(position);
+        holder.titulo .setText(filme.getTituloFilme());
+        holder.genero.setText(filme.getGenero());
+        holder.ano.setText(filme.getAno());
 
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+     return  listaFilmes.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
